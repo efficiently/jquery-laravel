cbc6b9d9c09579ce2601d46d5260a297273728a7
Tortue Torche <tortuetorche@spam.me>
2014-07-11 17:50:09 +0200

    Remove setupLayout() method which is already in BaseController class of your Laravel application.

--- src/Efficiently/JqueryLaravel/ControllerAdditions.php
+++ src/Efficiently/JqueryLaravel/ControllerAdditions.php
@@ -52,17 +52,6 @@ trait ControllerAdditions
         }
     }
 
-    /**
-     * Setup the layout used by the controller.
-     *
-     * @return void
-     */
-    protected function setupLayout()
-    {
-        if (! is_null($this->layout) && $this->layout !== false) {
-            $this->layout = View::make($this->layout);
-        }
-    }
 }
 
 // Use case:
