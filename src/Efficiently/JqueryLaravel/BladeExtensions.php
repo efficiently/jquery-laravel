bc7ba1a838383d3f122da64f583319171f453718
Tortue Torche <tortuetorche@spam.me>
2014-07-21 15:34:14 +0200

    Remove 'form' tag in Blade extensions.

--- src/Efficiently/JqueryLaravel/BladeExtensions.php
+++ src/Efficiently/JqueryLaravel/BladeExtensions.php
@@ -18,7 +18,7 @@ class BladeExtensions
                 "a", "abbr", "acronym", "address", "applet", "article", "aside", "audio", "b", "basefont", "bdi", "bdo",
                 "big", "blockquote", "body", "button", "canvas", "caption", "center", "cite", "code", "colgroup", "datalist",
                 "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "fieldset", "figcaption", "figure",
-                "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "html",
+                "font", "footer"/*, "form"*/, "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "html",
                 "i", "iframe", "ins", "kbd", "label", "legend", "li", "main", "map", "mark", "menu", "meter", "nav", "noframes",
                 "noscript", "object", "ol", "optgroup", "option", "output", "p", "pre", "progress", "q", "rp", "rt", "ruby", "s",
                 "samp", "script", "section", "select", "small", "span", "strike", "strong", "style", "sub", "summary", "sup",
