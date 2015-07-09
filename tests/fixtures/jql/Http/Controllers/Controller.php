3a85e4be68e7d0b95b4069055e23ffbe91e32ea5
Tortue Torche <tortuetorche@spam.me>
2015-07-09 15:44:32 +0200

    Use 'DispatchesCommands' instead of 'DispatchesJobs' trait in the Controller class to fix tests with Laravel 5.0.

--- tests/fixtures/jql/Http/Controllers/Controller.php
+++ tests/fixtures/jql/Http/Controllers/Controller.php
@@ -1,12 +1,12 @@
 <?php namespace Jql\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class Controller extends BaseController
 {
-    use DispatchesJobs, ValidatesRequests;
+    use DispatchesCommands, ValidatesRequests;
     use \Efficiently\JqueryLaravel\ControllerAdditions;
 
     /**
