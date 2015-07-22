b6e1513da09ea931b3f50ee245ef4c0d69ad3043
Tortue Torche <tortuetorche@spam.me>
2015-07-22 14:05:31 +0200

    Update ControllerAdditions trait.
    * Add a new redirectTo() method
    * Support the upcoming Turbolinks 3 via response macros.

--- src/Efficiently/JqueryLaravel/ControllerAdditions.php
+++ src/Efficiently/JqueryLaravel/ControllerAdditions.php
@@ -6,18 +6,40 @@ use Response;
 
 trait ControllerAdditions
 {
-
     /**
      * @param  \Illuminate\View\View|string|array $view View object or view path
      * @param  array $data Data that should be made available to the view. Only used when $view is a string path
-     * @param  string|boolean $layout Master layout path
+     * @param  mixed $options If $options is a string, it's used as the master layout path of the view.
+     *               E.G. $this->render('messages.index', 'app'); where the layout 'app' is the 'resources/views/app.blade.php' file.
+     *               If $options is null, the master layout is set with the value of the Controller::$layout property.
+     *               If $options is a boolean, if`it's `false`, no layout is applied on the view.
+     *               If $options is an array, default values are: ['status' => 200, 'headers' => []]
      *
      * @return \Illuminate\Http\Response
      */
-    public function render($view, $data = [], $layout = null)
+    public function render($view, $data = [], $options = null)
     {
-        $format = null;
+        $layout = null;
+        $defaultOptions = ['status' => 200, 'headers' => []];
+        if (is_string($options)) {
+            // To support legacy behaviour
+            $layout = $options;
+            $options = [];
+        } elseif (is_array($options)) {
+            if (array_key_exists('layout', $options)) {
+                $layout = $options['layout'];
+                unset($options['layout']);
+            }
+        } else {
+            if ($options === false) {
+                // To support legacy behaviour
+                $layout = false;
+            }
+            $options = [];
+        }
+        $options = array_merge($defaultOptions, $options);
 
+        $format = null; // if `null`, it uses the 'html' format by default
         if (is_array($view) && count($view) === 1) {
             $format = array_keys($view)[0];
             $view = array_values($view)[0];
@@ -32,12 +54,13 @@ trait ControllerAdditions
         }
 
         if (is_string($view) && view()->exists($view)) {
+            // Transform the $view string path to a View object
             $view = view($view, $data);
         }
 
         // short circuit
         if ($format) {
-            $response = response($view);
+            $response = response($view, $options['status'], $options['headers']);
             $response->header('Content-Type', Request::getMimeType($format));
 
             return $response;
@@ -48,13 +71,34 @@ trait ControllerAdditions
             $this->setupLayout();
         }
 
+        $render = $view;
         if ($this->layout) {
             $this->layout->content = $view;
-
-            return response($this->layout);
+            $render = $this->layout;
         }
 
-        return response($view);
+        if (response()->hasMacro('makeWithTurbolinks')) {
+            return response()->makeWithTurbolinks($render, $options);
+        }
+
+        return response()->make($render, $options['status'], $options['headers']);
+    }
+
+    /**
+     * @param  string $path URL
+     * @param  array $options Default to: ['status' => 302, 'headers' => [], 'secure' => null]
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectTo($path, $options = [])
+    {
+        $defaultOptions = ['status' => 302, 'headers' => [], 'secure' => null];
+        $options = array_merge($defaultOptions, $options);
+        if (response()->hasMacro('redirectToWithTurbolinks')) {
+            return response()->redirectToWithTurbolinks($path, $options);
+        }
+
+        return response()->redirectTo($path, $options['status'], $options['headers'], $options['secure']);
     }
 
     /**
